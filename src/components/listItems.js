337855263a526e283aed94d7ba63c1ed3c90c9b8
y-watanabe <y-watanabe@k-mit.jp>
2020-04-28 18:38:20 +0900

    環境構築＋少し実装

---
+++ src/components/listItems.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
+
+export default function NestedList(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <List
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      className={classes.root}
+    >
+      <ListItem button onClick={handleClick}>
+        <ListItemText primary={props.area.name} />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          {Object.keys(props.area.prefs).map(key => (
+            <ListItem button className={classes.nested}>
+              <ListItemText primary={props.area.prefs[key]} />
+            </ListItem>  
+          ))}
+        </List>
+      </Collapse>
+    </List>
+  );
+}
